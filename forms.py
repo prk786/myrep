8988b55b376dfc30d0693f36a097def508244f16
prk786 <48818281+prk786@users.noreply.github.com>
2019-03-27 06:49:48 -0700

    latest  edit in form

--- forms.py
+++ forms.py
@@ -29,7 +29,7 @@ class RegistrationForm(FlaskForm):
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
-            raise ValidationError(_('Please use the different email address.'))
+            raise ValidationError(_('Please use the different email address okay.'))
 
 
 class ResetPasswordRequestForm(FlaskForm):
